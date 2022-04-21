da59d979731af52f777e3552a02b0fd8bd6aac0e
hariom936 <hariomverma968@gmail.com>
2022-04-21 21:20:53 +0530

    Rollodex-app

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,18 @@
+import MenuItem from '../menu-item/MenuItem.component'
+import './directory.style.scss'
+
+const Directory = ({sections}) => {
+    return (
+        <>
+            <div className='directory-menu'>
+            {
+         
+                sections.map(menu => <MenuItem menu={menu} /> )
+            }
+               
+            </div>
+        </>
+    )
+}
+
+export default Directory
